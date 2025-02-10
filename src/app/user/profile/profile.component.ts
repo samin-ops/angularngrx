d501218235c130659bd076e8751f8458e48c8f86
serge samin <samexpertsg@gmail.com>
2025-02-10 20:21:20 +0000

    plusieurs facon de faire le formulaire

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,49 +1,109 @@
 import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { UserService } from '../store/user.services';
-import {toSignal} from '@angular/core/rxjs-interop'
+import { toSignal } from '@angular/core/rxjs-interop';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { User } from '../store/user.interface';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
 })
 export class ProfileComponent implements OnInit {
-
   private userService = inject(UserService);
   private fb = inject(FormBuilder);
   //user = signal({}) // signal
 
-  profileForm!: FormGroup
+  profileForm!: FormGroup;
 
-  user = toSignal(this.userService.getUser())
 
-  fullName = computed(()=>this.user()?.name.firstname + ' ' + this.user()?.name.lastname)
+  user = toSignal(this.userService.getUser());
 
-  ngOnInit(): void {
+  fullName = computed(
+    () => this.user()?.name.firstname + ' ' + this.user()?.name.lastname
+  );
+
+  // get addresses(): FormArray {
+  //   return this.profileForm.get('address') as FormArray
+  // }
+
+  ngOnInit() {
     this.profileForm = this.fb.group({
-      id: [this.user()?.id],
-      firstName: [this.user()?.name.firstname],
-      lastName: [this.user()?.name.lastname],
-      email: [this.user()?.email],
-      phone: [this.user()?.phone],
-      address: this.fb.group({  //nested form group
-        street: [this.user()?.address.street],
-        city: [this.user()?.address.city],
-        zipcode: [this.user()?.address.zipcode],
-        geolocation: this.fb.group({  //nested form group
-          latitude: [this.user()?.address.geolocation.lat],
-          longitude: [this.user()?.address.geolocation.long]
-        })
+      id: ['', Validators.required],
+      email: [''],
+      phone: [''],
+      name: this.fb.group({
+        firstname: ['', Validators.required],
+        lastname: [''],
       }),
-    })
-  }
+      // premiere facon de faire
+       address: this.fb.group({//nested form group
+        city: ['', Validators.required],
+        street: [''],
+        zipcode: [''],
+        number: [''],
+        geolocation: this.fb.group({//nested form group
+          lat: ['', Validators.required],
+          long: ['', Validators.required],
+        }),
+      }),
 
-  onSubmit(){
-    console.log(this.profileForm.value)
+      //address: this.fb.array([this.prepareAddressForm()]), //form array
+    });
+    this.loadProfile()
+
+  }
+ //deuxieme facon de faire
+// prepareAddressForm(){
+//      return this.fb.group({
+//       city: ['', Validators.required],
+//       street: [''],
+//       zipcode: [''],
+//       number: [''],
+//       geolocation: this.fb.group({
+//         lat: ['', Validators.required],
+//         long: ['', Validators.required],
+//       }),
+//     })
+//   }
+
+
+  // addAddress(){
+  //   this.addresses.push(this.prepareAddressForm())
+  // }
+  // resetAddress(){
+  //   this.addresses.clear()
+  // }
+  // removeAddress(index:number){
+  //   this.addresses.removeAt(index)
+  // }
+
+  loadProfile() {
+    this.userService.getUser().subscribe(user=>{
+      this.profileForm.patchValue(user)
+      console.log(user);
+
+    })
+
   }
 }
